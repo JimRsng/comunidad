7081ffa0f6f4d003009880e0e3fda02ef9e47895
Yizack Rangel <yizackr@gmail.com>
2026-08-28 02:55:17 +0200

    fix(router): reset scroll on navigation

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,7 +1,8 @@
 import type { RouterConfig } from "@nuxt/schema";
 
 export default {
-  scrollBehavior: (to) => {
+  scrollBehavior: (to, from) => {
     if (to.hash) return { el: to.hash, top: 56 };
+    else if (from.name) return { left: 0, top: 0 };
   }
 } satisfies RouterConfig;
